03a1c62632f0172b8b608f63f3508720f77542fd
5pence <sbarriball@me.com>
2018-11-02 01:06:48 +0000

    inital commit

---
+++ app3.js
@@ -0,0 +1,26 @@
+const http = new EasyHTTP;
+
+//Get Users
+// http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+
+// Post data
+const data = {
+    name: 'Spencer Barriball',
+    username: '5pence',
+    email: 'sbarriball@me.com'
+}
+
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
